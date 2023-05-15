9dfe2cf022ee156f1da569ab929f471a65067fbb
YadanarLin <106858849+gadatt@users.noreply.github.com>
2023-05-15 18:31:46 +0900

    updates

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,20 +1,36 @@
 import Image from 'next/image'
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <p>Contact Information</p>
+      <p>https://github.com/YadanarLin</p>
+	  <p> https://github.com/gadatt</p>
+	  <p>yadanarrlinn@gmail.com</p>
+	  <p>080-7197-1517</p>
+	  <p>linkedin.com/in/yadanarlin</p><a href="https://linkedin.com/in/yadanarlin"></a>
 	  </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <h3 className={`mb-3 text-2xl font-semibold`}>Yadanar Lin</h3>
-      </div>
-
+      <div className="relative flex place-items-center">
+        <p className={`mb-3 text-2xl font-semibold`}>Yadanar Lin</p>
+	</div>
+		<div>
+		<p>
+			Mingalarbar!<br /> I am a girl from Yangon, Myanmar, with a huge heart for coding. <br />  Explore the contents below to know more about me. 
+		</p>
+		</div>
+	
+		<div>
+		<p>
+			Education: 3rd Year Computer Science Under graduate at Toyo University.
+			<br /> A full scholarship recipient.
+		</p>
+		</div>
+		
+      
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        <Link
+          href="/pages/projects"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -26,11 +42,10 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
           </p>
-        </a>
+        </Link>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+        <Link
+          href="/pages/work"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -40,35 +55,33 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        </Link>
 
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
+          href="/pages/awards"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+		  target="_blank"
+          rel="noopener noreferrer">
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Personal Timeline{' '}
+           Certifications & <br></br>
+		   Awards{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
           </p>
-        </a>
+        </Link>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+		<a
+          href="https://www.facebook.com/profile.php?id=100084353844354"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-           Hobbies{' '}
+			Personal Blog
+		   {' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
