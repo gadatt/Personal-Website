d93fda87ac5bcf5edf41288becb4a77b077bd521
YadanarLin <106858849+gadatt@users.noreply.github.com>
2023-05-15 21:14:20 +0900

    new app + base set up

--- ylin/pages/index.tsx
+++ ylin/pages/index.tsx
@@ -1,36 +1,33 @@
 import Image from 'next/image'
-import Link from 'next/link';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <p>https://github.com/YadanarLin</p>
-	  <p> https://github.com/gadatt</p>
-	  <p>yadanarrlinn@gmail.com</p>
-	  <p>080-7197-1517</p>
-	  <p>linkedin.com/in/yadanarlin</p><a href="https://linkedin.com/in/yadanarlin"></a>
-	  </div>
-
-      <div className="relative flex place-items-center">
-        <p className={`mb-3 text-2xl font-semibold`}>Yadanar Lin</p>
-	</div>
-		<div>
-		<p>
-			Mingalarbar!<br /> I am a girl from Yangon, Myanmar, with a huge heart for coding. <br />  Explore the contents below to know more about me. 
-		</p>
-		</div>
-	
-		<div>
-		<p>
-			Education: 3rd Year Computer Science Under graduate at Toyo University.
-			<br /> A full scholarship recipient.
-		</p>
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+		<div  className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-center">
+		<p>yadanarrlinn@gmail.com</p>
+		<p>080-7197-1517</p>
+		<a href="https://linkedin.com/in/yadanarlin"
+		target="_blank"
+		rel="noopener noreferrer"><p>LinkedIn</p></a>
+		<a href="https://github.com/gadatt"
+		target="_blank"
+		rel="noopener noreferrer"><p>GitHub</p></a>
 		</div>
 		
+       <div><h1>Yadanar Lin</h1></div>
+	   <p>Mingalarbar! <br></br>I am a girl from Myanmar with a huge heart for coding.
+	   <br /> Explore below to know more about me!</p>
       
+
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link
-          href="/pages/projects"
+        <a
+          href="/projects"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -40,54 +37,49 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          </p>
-        </Link>
+        </a>
 
-        <Link
-          href="/pages/work"
+        <a
+          href="/work"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-           Work History{' '}
+            Work History{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-        </Link>
+        </a>
 
-        <Link
-          href="/pages/awards"
+        <a
+          href="/awards"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-		  target="_blank"
-          rel="noopener noreferrer">
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-           Certifications & <br></br>
-		   Awards{' '}
+		  Awards & Certifications{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          </p>
-        </Link>
+    
+        </a>
 
-		<a
+        <a
           href="https://www.facebook.com/profile.php?id=100084353844354"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-			Personal Blog
-		   {' '}
+           Personal Blog{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          </p>
         </a>
       </div>
     </main>
